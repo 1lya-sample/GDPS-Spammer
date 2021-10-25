27923f4d14c95e8d6b61a17ec0dc25f183634f86
1lyasample <81309334+1lyasample@users.noreply.github.com>
2021-10-25 09:55:49 +0300

    Update levels.py

--- levels.py
+++ levels.py
@@ -2,13 +2,11 @@ import requests
 from random import randint as rand
 from base64 import b64encode as b64e
 
-def postLevel(levelName, levelDesc, levelLength, audioTrack, levelString, udid):
+def postLevel(levelName, userName, levelDesc, levelLength, audioTrack, levelString, udid):
     endpoint = database
-    userName = userName
-    levelName = levelName
     data = {
         'gameVersion': 21,
-        'userName': userName+str(rand(111111111, 999999999)),
+        'userName': userName,
         'gjp': '',
         'levelID': 0,
         'secret': 'Wmfv3899gc9',
@@ -20,7 +18,8 @@ def postLevel(levelName, levelDesc, levelLength, audioTrack, levelString, udid):
         'levelString': levelString,
         'udid': udid
     }
-    r = requests.post(endpoint+"uploadGJLevel21.php", data=data)
+    headers = {'User-Agent': ''}
+    r = requests.post(endpoint+"uploadGJLevel21.php", data=data, headers=headers)
     return r.text
 def generateUDID():
     udid = "S"+str(rand(111111111, 999999999))+str(rand(111111111, 999999999))+str(rand(111111111, 999999999))+str(rand(111111111, 999999999))+str(rand(1,9))
@@ -28,8 +27,9 @@ def generateUDID():
 
 print("Sample's GDPS Level Spammer") 
 database = input("database url (for example: http://ps.fhgdps.com/Geometrykras/): ")
-userName = input("custom username before numbers: ")
-levelName = input("custom levelname before numbers: ")
+userName = input("custom username: ")
+levelName = input("custom levelname: ")
+levelDesc = input("custom description: ")
 while True:
-    l = postLevel("raid"+str(rand(11111,9999999)), "", 3, 4, "fwfwefwefwwef"+str(rand(111111111, 999999999)), generateUDID())
+    l = postLevel(levelName, userName, levelDesc, 3, 4, "fwfwefwefwwef"+str(rand(111111111, 999999999)), generateUDID())
     print(l)
